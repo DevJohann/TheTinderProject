da92dbb7b80edc9dd8b55dff0cf9363730c91b89
Maubg2 <mauriciobel2002@gmail.com>
2022-05-17 18:00:31 -0500

    Cosas

--- src/co/edu/unbosque/thetinderproject/view/MainView.java
+++ src/co/edu/unbosque/thetinderproject/view/MainView.java
@@ -1,5 +1,6 @@
 package co.edu.unbosque.thetinderproject.view;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -13,7 +14,8 @@ public class MainView {
 	
 	JButton loginButton = new JButton("Login");
 	JButton registerButton = new JButton("Register");
-	JLabel mainMessajeLabel = new JLabel("Tinder");
+	JLabel mainMessageLabel = new JLabel("TINDER");
+	JLabel footer = new JLabel("By: Crypted Security ©");
 	JFrame mainFrame = new JFrame();
 	
 	public MainView() {
@@ -23,8 +25,22 @@ public class MainView {
 		mainFrame.setTitle("Tinder");
 		
 		//LoginButton section
-		loginButton.setBounds(120, 120, 120, 40);
+		loginButton.setBounds(180, 130, 120, 40);
 		mainFrame.add(loginButton);
+		
+		//RegisterButton section
+		registerButton.setBounds(180, 190, 120, 40);
+		mainFrame.add(registerButton);
+		
+		//MainMessajeLabel section
+		mainMessageLabel.setBounds(125, 20, 400, 60);
+		mainMessageLabel.setFont(new Font("Serif", Font.BOLD, 60));
+		mainFrame.add(mainMessageLabel);
+		
+		//Footer section
+		footer.setBounds(182, 400, 150, 40);
+		footer.setFont(new Font("Serif", Font.ITALIC, 12));
+		mainFrame.add(footer);
 	}
 	public JButton getLoginButton() {
 		return this.loginButton;
