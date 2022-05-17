440b796a0a0e79905004c6d81133cea9e8bf6a7c
Maubg2 <mauriciobel2002@gmail.com>
2022-05-17 18:23:43 -0500

    getters login

--- src/co/edu/unbosque/thetinderproject/view/LoginView.java
+++ src/co/edu/unbosque/thetinderproject/view/LoginView.java
@@ -1,5 +1,34 @@
 package co.edu.unbosque.thetinderproject.view;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class LoginView {
+	
+	JLabel nameLabelLogin;
+	JLabel passwordLabelLogin;
+	JTextField nameFieldLogin;
+	JTextField passwordFieldLogin;
+	JButton sendDataButtonLogin;
+	
+	public void showLoginView() {
+		
+	}
+	public String getNameTFieldLogin() {
+		return nameFieldLogin.getText();
+	}
+	public String getPasswordTFieldLogin() {
+		return passwordFieldLogin.getText();	
+	}
+	public JButton getSendDataButtonLogin() {
+		return sendDataButtonLogin;
+	}
 
 }
