986fd9700e1f3a9523a1df2caf5cf5a4d8f24c14
DevJohann <felipepoveda@gmail.com>
2022-05-17 18:23:38 -0500

    x

--- src/co/edu/unbosque/thetinderproject/controller/MainController.java
+++ src/co/edu/unbosque/thetinderproject/controller/MainController.java
@@ -5,13 +5,24 @@ import java.awt.event.ActionListener;
 import co.edu.unbosque.thetinderproject.view.*;
 
 public class MainController {
-	public MainController(MainView view, LoginView loginView) {
+	public MainController(MainView view, LoginView loginView, RegisterView registerView) {
+		//Login Button
 		view.getLoginButton().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				view.getMainFrame().getContentPane().removeAll();
 				view.getMainFrame().repaint();
 				//loginView.show();
+			}
+		});
+		
+		//Register Button
+		view.getRegisterButton().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				view.getMainFrame().getContentPane().removeAll();
+				view.getMainFrame().repaint();
+				//registerView.show();
 			}
 		});
 	}
--- src/co/edu/unbosque/thetinderproject/view/RegisterView.java
+++ src/co/edu/unbosque/thetinderproject/view/RegisterView.java
@@ -1,5 +1,25 @@
 package co.edu.unbosque.thetinderproject.view;
 
-public class RegisterView {
+import javax.swing.*;
 
+public class RegisterView {
+	JLabel nameLabelRegister = new JLabel("Usuario:");
+	JLabel passwordLabelRegister = new JLabel("Contraseña:");
+	JTextField nameFieldRegister = new JTextField();
+	JTextField passwordFieldRegister = new JTextField();
+	JButton sendDataButtonRegister = new JButton("Enviar");
+	
+	public void showRegisterView() {
+		
+	}
+	
+	public String getPasswordRegister() {
+		return this.passwordFieldRegister.getText();
+	}
+	public String getNameRegister() {
+		return this.nameFieldRegister.getText();
+	}
+	public JButton getSendDataButtonRegister() {
+		return this.sendDataButtonRegister;
+	}
 }
