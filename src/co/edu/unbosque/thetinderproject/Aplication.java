37a07dcbbb65b6581c5e9e2da35a1a4673c8f11c
DevJohann <felipepoveda@gmail.com>
2022-05-17 17:28:43 -0500

    Fusion

--- src/co/edu/unbosque/thetinderproject/Aplication.java
+++ src/co/edu/unbosque/thetinderproject/Aplication.java
@@ -1,11 +1,16 @@
 package co.edu.unbosque.thetinderproject;
 
+import co.edu.unbosque.thetinderproject.controller.MainController;
+import co.edu.unbosque.thetinderproject.view.LoginView;
+import co.edu.unbosque.thetinderproject.view.MainView;
+
 public class Aplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainView view = new MainView();
-		MainController controller = new MainController(view);
+		LoginView loginView = new LoginView();
+		MainController controller = new MainController(view, loginView);
 	}
 
 }
