d09b139ec71cc8eb3544b48fed7e5b062d9cfaa0
DevJohann <felipepoveda@gmail.com>
2022-05-17 19:42:46 -0500

    Final Merge

--- src/co/edu/unbosque/thetinderproject/view/PreferencesView.java
+++ src/co/edu/unbosque/thetinderproject/view/PreferencesView.java
@@ -1,5 +1,19 @@
 package co.edu.unbosque.thetinderproject.view;
 
+import javax.swing.*;
+
 public class PreferencesView {
 	//Programadora: Jessica
+	JCheckBox mainCheckBoxPView = new JCheckBox();
+	JButton backButtonPreferencesView = new JButton("Volver");
+	
+	public void showPreferencesView() {
+		
+	}
+	//public String getCheckBoxValuePView() {
+		//return this.mainCheckBoxPView.getSelectedIcon();
+	//}
+	public JButton getBackButtonPreferences() {
+		return this.backButtonPreferencesView;
+	}
 }
