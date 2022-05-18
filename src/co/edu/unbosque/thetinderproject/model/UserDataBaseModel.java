7566ede083ac42577adeb47e030063dc7d2e0e1d
DevJohann <felipepoveda@gmail.com>
2022-05-17 20:29:07 -0500

    Final merge

--- src/co/edu/unbosque/thetinderproject/model/UserDataBaseModel.java
+++ src/co/edu/unbosque/thetinderproject/model/UserDataBaseModel.java
@@ -1,7 +1,25 @@
 package co.edu.unbosque.thetinderproject.model;
 
+import java.util.ArrayList;
+import co.edu.unbosque.thetinderproject.model.*;
+
 public class UserDataBaseModel {
 
 	//Programmer: Johann
+	ArrayList<UserModel> UserDB = new ArrayList<UserModel>();
 	
+	public UserModel searchUser(String username) {
+		for(UserModel user : UserDB) {
+			if(user.getUsernameUM().equals()) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	public void addUser(String username, String password) {
+		UserDB.add(new UserModel(username, password));
+	}
+	
+	//JFrame.setLocationRelativeTo(null)
 }
