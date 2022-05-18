fe36e9908c3894fe8d77989213ac7fbba21c4d02
jessi2072 <jessicamilenabogotapadilla00@gmail.com>
2022-05-17 20:20:24 -0500

    Jess , cambios en ValidateUserModel

--- src/co/edu/unbosque/thetinderproject/model/ValidateUserModel.java
+++ src/co/edu/unbosque/thetinderproject/model/ValidateUserModel.java
@@ -1,6 +1,13 @@
 package co.edu.unbosque.thetinderproject.model;
 
 public class ValidateUserModel {
-
 	//Programmer: Jessica
+	
+	String username;
+	String password;
+	UserDataBaseModel userDataBase;
+	
+	public boolean ValidateUser() {
+		return false;
+	}
 }
