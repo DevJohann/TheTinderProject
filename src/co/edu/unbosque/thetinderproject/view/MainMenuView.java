159ebce95b78a4f87787feb00c2e491dc074dbd5
FHER94 <jpenagosb@unbosque.edu.co>
2022-05-17 19:17:13 -0500

    tres declaraciones solamente

--- src/co/edu/unbosque/thetinderproject/view/MainMenuView.java
+++ src/co/edu/unbosque/thetinderproject/view/MainMenuView.java
@@ -1,5 +1,21 @@
 package co.edu.unbosque.thetinderproject.view;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
 public class MainMenuView {
 	//Programadora: Jennifer
-}
+	
+	JLabel workingUserLabelMMView;
+	JLabel mainTextLabelMMView;
+	JButton LogOutButtonMMView;
+	
+	
+		
+		
+		
+		
+	}
+
+
+
