278031c69aa15e568683c554c55e3e71715ea18d
Maubg2 <mauriciobel2002@gmail.com>
2022-05-17 19:42:05 -0500

    lo que sea

--- src/co/edu/unbosque/thetinderproject/view/MainMenuView.java
+++ src/co/edu/unbosque/thetinderproject/view/MainMenuView.java
@@ -1,14 +1,20 @@
 package co.edu.unbosque.thetinderproject.view;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 public class MainMenuView {
 	//Programadora: Jennifer
 	
-	JLabel workingUserLabelMMView;
-	JLabel mainTextLabelMMView;
-	JButton LogOutButtonMMView;
+	JLabel workingUserLabelMMView = new JLabel();
+	JLabel mainTextLabelMMView = new JLabel("Menu principal");
+	JButton LogOutButtonMMView = new JButton("Cerrar sesión");
+	
+	public JButton getLogOutButtonMMView() {
+		return LogOutButtonMMView;
+	}
+	public JLabel setWorkingUserLabelMMView() {
+		return workingUserLabelMMView;
+	}
 	
 	
 		
