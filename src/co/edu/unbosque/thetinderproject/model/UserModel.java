648d956ebc0a308dc145822dd5908611a98f1c79
Maubg2 <mauriciobel2002@gmail.com>
2022-05-17 20:28:03 -0500

    Holaaaa

--- src/co/edu/unbosque/thetinderproject/model/UserModel.java
+++ src/co/edu/unbosque/thetinderproject/model/UserModel.java
@@ -3,4 +3,76 @@ package co.edu.unbosque.thetinderproject.model;
 public class UserModel {
 	
 	//Programmer: Mauricio
+	
+	private String name;
+	private String username;
+	private String password;
+	private String picture;
+	private String nacionality;
+	private String cityOfResidence;
+	private int age;
+	private String gender;
+	private String description;
+	private boolean firstTimer;
+	
+	public UserModel(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	public String getNameUM() {
+		return name;
+	}
+	public String getUsernameUM() {
+		return username;
+	}
+	public String getPasswordUM() {
+		return password;
+	}
+	public String getPictureUM() {
+		return picture;
+	}
+	public String getNacionalityUM() {
+		return nacionality;
+	}
+	public String getCityOfResidenceUM() {
+		return cityOfResidence;
+	}
+	public int getAgeUM() {
+		return age;
+	}
+	public String getGenderUM() {
+		return gender;
+	}
+	public String getDescriptionUM() {
+		return description;
+	}
+	public boolean getFirstTimerUM() {
+		return firstTimer;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public void setNacionality(String nacionality) {
+		this.nacionality = nacionality;
+	}
+	public void setCityOfResidence(String cityOfResidence) {
+		this.cityOfResidence = cityOfResidence;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setFirstTimer(boolean firstTimer) {
+		this.firstTimer = firstTimer;
+	}
+	
+	
 }
